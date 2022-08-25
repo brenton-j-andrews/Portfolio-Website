86d4de06dd3270be11fd912e189a230018112565
Brenton Andrews <brenton.j.andrews@gmail.com>
2022-08-25 12:40:09 -0600

    Responssive nav bar completed, dropdown feature may be added in the future.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,22 +1,42 @@
 import React from "react";
-import moon from "../Assets/Images/moon.png";
-import sun from "../Assets/Images/sun.png";
+import moon from "../Assets/Images/Icons/moon.png";
+import sun from "../Assets/Images/Icons/sun.png";
+import menu from "../Assets/Images/Icons/menu.png";
+import white_menu from "../Assets/Images/Icons/white_menu.png";
 
 const Navbar = ({darkMode, setDarkMode}) => {
+
+    function toggleDropdownNav() {
+        let bar = document.querySelector(".primary-nav-bar");
+        if (bar.className === "primary-nav-bar") {
+            bar.className += " responsive";
+        } else {
+            bar.className = "primary-nav-bar";
+        }
+    }
+
+
     return (
         <nav className="primary-nav-bar">
 
-            <div className="nav-bar-left">
-                <a href="/" className="test">Home</a>
-            </div>
+            <a className="home-link" href="/">Home</a>
 
             <div className="nav-bar-right">
                 <a href="/about">About</a>
                 <a href="/projects">Projects</a>
+                <a href="/contact">Contact</a>
                 <a href="/hobbies">Hobbies</a>
-                <button onClick={() => {setDarkMode(!darkMode)}}> { darkMode ? <img src={moon} alt="dark mode"></img> :  <img src={sun} alt="light mode"></img> }  </button>
+
+                <button onClick={() => {setDarkMode(!darkMode)}} className="dark-mode-toggle"> 
+                    { darkMode ? <img src={moon} alt="dark mode"></img> :  <img src={sun} alt="light mode"></img> }  
+                </button>
             </div>
-            
+
+
+            <button onClick={() => toggleDropdownNav()} className="reactive-bar"> 
+                { darkMode ? <img src={menu} alt="dark mode"></img> :  <img src={white_menu} alt="light mode"></img> }  
+            </button>
+
         </nav>
     )
 }
